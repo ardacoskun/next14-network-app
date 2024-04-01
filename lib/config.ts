3a1dab65f65e0e7fe0b4859c8ffa7c2b0d31f19b
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-01 23:16:48 +0300

    next auth config file added

---
+++ lib/config.ts
@@ -0,0 +1,14 @@
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+const config = {
+  POSTGRES_URL: process.env.POSTGRES_URL!,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+  GITHUB_ID: process.env.GITHUB_ID!,
+  GITHUB_SECRET: process.env.GITHUB_SECRET!,
+  APP_ENV: process.env.APP_ENV!,
+};
+
+export default config;
