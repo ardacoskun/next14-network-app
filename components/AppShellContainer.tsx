709d74bdf0d151cfcc44bc7d12d41cf1b0756836
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-05 20:58:50 +0300

    header menu and sign out btn added

--- components/AppShellContainer.tsx
+++ components/AppShellContainer.tsx
@@ -1,8 +1,15 @@
 "use client";
 import { ReactNode } from "react";
 import { usePathname } from "next/navigation";
-import { AppShell, Burger } from "@mantine/core";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { AppShell, Avatar, Burger, Menu } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import {
+  IconDoorExit,
+  IconSettings,
+  IconUserCircle,
+} from "@tabler/icons-react";
 
 const AppShellContainer = ({ children }: { children: ReactNode }) => {
   const [opened, { toggle }] = useDisclosure();
@@ -24,6 +31,27 @@ const AppShellContainer = ({ children }: { children: ReactNode }) => {
               size="sm"
             />
             Professional Network
+          </div>
+          <div className="flex justify-end gap-5 h-full items-center p-5">
+            <div>Search</div>
+            <div>
+              <Menu>
+                <Menu.Target>
+                  <Avatar>
+                    <IconUserCircle />
+                  </Avatar>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Label>Menu</Menu.Label>
+                  <Menu.Item leftSection={<IconSettings />}>
+                    <Link href="/dashboard/profile">Edit Profile</Link>
+                  </Menu.Item>
+                  <Menu.Item leftSection={<IconDoorExit />}>
+                    <button onClick={() => signOut()}>Sign out</button>
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </div>
           </div>
         </div>
       </AppShell.Header>
