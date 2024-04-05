f8966892033dcfdb9429aeccf55feff856902f77
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-05 20:52:57 +0300

    app shell container added

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "@/providers/SessionProvider";
+import AppShellContainer from "@/components/AppShellContainer";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const session = await getSession();
@@ -10,5 +11,9 @@ export default async function Layout({ children }: { children: ReactNode }) {
     redirect("/signin");
   }
 
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      <AppShellContainer>{children}</AppShellContainer>
+    </SessionProvider>
+  );
 }
---
+++ components/AppShellContainer.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+const AppShellContainer = ({ children }: { children: ReactNode }) => {
+  const [opened, { toggle }] = useDisclosure();
+  const pathname = usePathname();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <div className="flex justify-between h-full">
+          <div className="flex h-full items-center gap-5 p-5">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            Professional Network
+          </div>
+        </div>
+      </AppShell.Header>
+      <AppShell.Navbar>navbar</AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+};
+
+export default AppShellContainer;
