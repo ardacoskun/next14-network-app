e3115f3b97fc15cb4ff2194b0475e21c1dcc0661
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-06 13:12:23 +0300

    seed job title script added

---
+++ scripts/seedJobTitle.ts
@@ -0,0 +1,26 @@
+import { eq } from "drizzle-orm";
+import { db, pool } from "@/lib/db";
+import { users } from "@/lib/schema";
+
+const JOB_TITLES = [
+  "Full Stack Developer",
+  "Back End Developer",
+  "Front End Developer",
+  "Project Manager",
+  "DevOps Engineer",
+  "Data Scientist",
+];
+
+async function main() {
+  const res = await db.query.users.findMany();
+  for (let user of res) {
+    const randomIdx = Math.floor(Math.random() * JOB_TITLES.length);
+    await db
+      .update(users)
+      .set({ jobTitle: JOB_TITLES[randomIdx] })
+      .where(eq(users.id, user.id));
+  }
+  pool.end();
+}
+
+main();
