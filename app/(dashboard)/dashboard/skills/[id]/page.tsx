b9050e4df01b07639ec822b5310bea9af56fef26
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-07 00:39:00 +0300

    sort functionality added

--- app/(dashboard)/dashboard/skills/[id]/page.tsx
+++ app/(dashboard)/dashboard/skills/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { db } from "@/lib/db";
 import { skills, users, usersToSkills } from "@/lib/schema";
 import SortSkillSelect from "@/components/skill/SortSkillSelect";
@@ -8,12 +8,29 @@ const getSkill = async (id: string) => {
   return await db.query.skills.findFirst({ where: eq(skills.id, id) });
 };
 
-const getUsers = async (skillId: string) => {
-  return await db
+const getUsers = async (skillId: string, sort: string) => {
+  const promise = db
     .select()
     .from(users)
     .leftJoin(usersToSkills, eq(users.id, usersToSkills.userId))
     .where(eq(usersToSkills.skillId, skillId));
+
+  switch (sort) {
+    case "name":
+      promise.orderBy(users.name);
+      break;
+    case "-name":
+      promise.orderBy(desc(users.name));
+      break;
+    case "rating":
+      promise.orderBy(usersToSkills.rating);
+      break;
+    case "-rating":
+      promise.orderBy(desc(usersToSkills.rating));
+      break;
+  }
+
+  return await promise;
 };
 
 interface PageProps {
@@ -30,7 +47,7 @@ const Page = async ({ params, searchParams }: PageProps) => {
   const { sort } = searchParams;
 
   const skill = await getSkill(id);
-  const users = await getUsers(id);
+  const users = await getUsers(id, sort);
 
   if (!id) {
     return notFound();
