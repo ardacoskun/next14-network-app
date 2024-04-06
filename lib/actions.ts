c724a9be36b1ed735b9c5948ed536ec4999a355f
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-07 02:29:08 +0300

    update user action added

---
+++ lib/actions.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { z } from "zod";
+import { getSession } from "./auth";
+import { db } from "./db";
+import { users } from "./schema";
+import { eq } from "drizzle-orm";
+
+const UpdateUserSchema = z.object({
+  jobTitle: z.string().min(3),
+  bio: z.string(),
+});
+
+interface UpdateUserState {
+  errors?: {
+    jobTitle?: string[];
+    bio?: string[];
+  };
+  message?: string;
+  success?: string;
+}
+
+export const updateUser = async (
+  prevState: UpdateUserState,
+  formData: FormData
+): Promise<UpdateUserState> => {
+  const session = await getSession();
+  const jobTitle = formData.get("jobTitle");
+  const bio = formData.get("bio");
+
+  const validateFields = UpdateUserSchema.safeParse({
+    jobTitle,
+    bio,
+  });
+
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: "Update user error",
+    };
+  }
+
+  await db
+    .update(users)
+    .set({
+      jobTitle: validateFields.data.jobTitle,
+      bio: validateFields.data.bio,
+    })
+    .where(eq(users.id, session?.user.id));
+
+  return {
+    success: "Update user success",
+  };
+};
