6115ccc79317cd2598e7f1a39f0c4a0d2b13ab8b
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-06 13:27:33 +0300

    get searchParams in people page

--- app/(dashboard)/dashboard/people/page.tsx
+++ app/(dashboard)/dashboard/people/page.tsx
@@ -1,12 +1,19 @@
 import { db } from "@/lib/db";
 
-const getUsers = async () => {
+const getUsers = async (page: number) => {
   const res = await db.query.users.findMany();
   return res;
 };
 
-const Page = async () => {
-  const data = await getUsers();
+interface PageProps {
+  searchParams: {
+    page: number;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { page } = searchParams;
+  const data = await getUsers(page || 1);
 
   return (
     <div>
