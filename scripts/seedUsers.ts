2553a7d5342fbb0d08aa8a1596a8f1f3498967e4
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-06 13:06:26 +0300

    seed users script added

---
+++ scripts/seedUsers.ts
@@ -0,0 +1,26 @@
+import { faker } from "@faker-js/faker";
+import { NewUser } from "../lib/types";
+import { users } from "../lib/schema";
+import { db, pool } from "../lib/db";
+
+async function main() {
+  for (let i = 0; i < 100; i++) {
+    const username = faker.internet.userName();
+    const email = `${username}@example.com`;
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const image = faker.image.avatarGitHub();
+    const newUser: NewUser = {
+      id: crypto.randomUUID(),
+      name: username,
+      email,
+      image,
+      firstName,
+      lastName,
+    };
+    await db.insert(users).values(newUser);
+  }
+  pool.end();
+}
+
+main();
