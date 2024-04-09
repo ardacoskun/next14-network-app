4e1e6283c42eee70e279f0d3f329ee741994dc04
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-10 01:47:22 +0300

    manage skills page table styles added

--- app/(dashboard)/dashboard/profile/skills/page.tsx
+++ app/(dashboard)/dashboard/profile/skills/page.tsx
@@ -1,7 +1,7 @@
+import { eq } from "drizzle-orm";
 import { getSession } from "@/lib/auth";
 import { db } from "@/lib/db";
-import { users, usersToSkills } from "@/lib/schema";
-import { eq } from "drizzle-orm";
+import { usersToSkills } from "@/lib/schema";
 
 const getUsersToSkills = async (userId: string) => {
   return await db.query.usersToSkills.findMany({
@@ -14,23 +14,36 @@ const Page = async () => {
   const session = await getSession();
   const usersToSkills = await getUsersToSkills(session?.user.id);
 
+  const tableHead = [
+    { id: 1, name: "Name" },
+    { id: 2, name: "Rating" },
+    { id: 3, name: "Delete" },
+  ];
+
   return (
-    <div>
-      <h1>Manage Skills</h1>
-      <table>
+    <div className="flex flex-col gap-5 max-w-xl">
+      <h1 className="font-bold text-xl">Manage Skills</h1>
+      <table className="border-collapse border-0">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Rating</th>
-            <th>Delete</th>
+            {tableHead.map((item) => (
+              <th
+                key={item.id}
+                className="border-slate-600 p-2 border-b text-left"
+              >
+                {item.name}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
           {usersToSkills.map((item) => (
             <tr key={item.skill.id}>
-              <td>{item.skill.name}</td>
-              <td>{item.rating}</td>
-              <td>Delete</td>
+              <td className="border-slate-600 p-2 border-t">
+                {item.skill.name}
+              </td>
+              <td className="border-slate-600 p-2 border-t">{item.rating}</td>
+              <td className="border-slate-600 p-2 border-t">Delete</td>
             </tr>
           ))}
         </tbody>
