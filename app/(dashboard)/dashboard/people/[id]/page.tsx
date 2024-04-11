759d4b622a38f38d650eed05c4b43f84dfcc05b5
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-11 14:24:46 +0300

    user card added into user detail page

--- app/(dashboard)/dashboard/people/[id]/page.tsx
+++ app/(dashboard)/dashboard/people/[id]/page.tsx
@@ -1,7 +1,31 @@
-const Page = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+import { notFound } from "next/navigation";
+import { eq } from "drizzle-orm";
+import { db } from "@/lib/db";
+import { users } from "@/lib/schema";
+import UserCard from "@/components/user/UserCard";
 
-  return <div>{id}</div>;
+const getUserById = async (id: string) => {
+  return await db.query.users.findFirst({
+    where: eq(users.id, id),
+    with: { usersToUsersSkills: { with: { skill: true } } },
+  });
+};
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const user = await getUserById(id);
+
+  if (!user) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <div>
+        <UserCard user={user} />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
