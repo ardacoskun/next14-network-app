c653faf488ed4208bd5f9c458c9785dae35e9a4a
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-13 01:06:09 +0300

    for dynamic to seed apis

--- app/api/scripts/hello/route.ts
+++ app/api/scripts/hello/route.ts
@@ -1,5 +1,7 @@
-import hello from "@/scripts/hello";
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
+import hello from "@/scripts/hello";
 
 export function GET() {
   hello();
